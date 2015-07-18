3879360867c8feacc8795f51ba5eff9594eed4e3
KrauseFx <KrauseFx@gmail.com>
2015-07-18 17:54:40 +0200

    Added error handling and group support

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -1,19 +1,26 @@
 class InviteController < ApplicationController
   def index
-
+    if user and password
+      # default
+    else
+      render 'error'
+    end
   end
 
   def submit
     email = params[:email]
     first_name = params[:last_name]
     last_name = params[:first_name]
+    group_name = ENV["ITC_GROUP_NAME"] || "Boarding"
 
     logger.info "Going to create a new tester: #{email} - #{first_name} #{last_name}"
 
-    user = ENV["ITC_USER"] || ENV["FASTLANE_USER"]
-    password = ENV["ITC_PASSWORD"] || ENV["FASTLANE_PASSWORD"]
     Spaceship::Tunes.login(user, password)
-    tester = Spaceship::Tunes::Tester::External.create!(email: email, first_name: first_name, last_name: last_name)
+
+    tester = Spaceship::Tunes::Tester::External.create!(email: email, 
+                                                        first_name: first_name, 
+                                                        last_name: last_name,
+                                                        group: group_name)
 
     logger.info "Successfully created tester #{email}"
 
@@ -23,4 +30,13 @@ class InviteController < ApplicationController
       logger.info "No app to add this tester to provided, use the `APP_ID` environment variable"
     end
   end
+
+  private
+    def user
+      ENV["ITC_USER"] || ENV["FASTLANE_USER"]
+    end
+
+    def password
+      ENV["ITC_PASSWORD"] || ENV["FASTLANE_PASSWORD"]
+    end
 end
