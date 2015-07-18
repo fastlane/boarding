8b5e069c5999e257c598a01ac9d8b21171e8110a
KrauseFx <KrauseFx@gmail.com>
2015-07-18 20:11:23 +0200

    Enabled logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,10 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+
+  config.logger = Logger.new(STDOUT) # from https://stackoverflow.com/questions/8098429/heroku-logging-not-working
+
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
