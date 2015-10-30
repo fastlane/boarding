9ef3add5f03500bd51644d2515a33d5657f8d4ff
Richard Lee <dlackty@gmail.com>
2015-10-30 21:48:44 +0800

    Add Heroku config

---
+++ config/puma.rb
@@ -0,0 +1,9 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
