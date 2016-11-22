2c47c362c2508b9e651afe3a6d69e5bed95bf26d
Benjamin Briggs <ben@palringo.com>
2016-11-22 17:31:29 +0000

    Add an optional cap for the number of testers

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -72,6 +72,16 @@ class InviteController < ApplicationController
     begin
       login
 
+      if ENV['MAX_NUMBER_OF_TESTERS']
+          if ENV['MAX_NUMBER_OF_TESTERS'].to_i <= Spaceship::Tunes::Tester::External.all.count
+              @message = t(:message_max_testers)
+              @type = 'warning'
+              render :index
+              #ENV['ITC_CLOSED_TEXT'] = t(:message_max_testers)
+              return
+          end
+      end
+
       tester = Spaceship::Tunes::Tester::External.find_by_app(apple_id, email)
 
       logger.info "Found tester #{tester}"
@@ -101,7 +111,7 @@ class InviteController < ApplicationController
         end
         @type = "success"
       end
-      
+
     rescue => ex
       Rails.logger.fatal ex.inspect
       Rails.logger.fatal ex.backtrace.join("\n")
